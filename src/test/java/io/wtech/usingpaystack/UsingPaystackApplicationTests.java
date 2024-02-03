0f360b15f1b46f4897dc9c68fb33d73fdf8aacec
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2024-02-03 20:25:41 +0100

    ** fix test

--- src/test/java/io/wtech/usingpaystack/UsingPaystackApplicationTests.java
+++ src/test/java/io/wtech/usingpaystack/UsingPaystackApplicationTests.java
@@ -2,12 +2,31 @@ package io.wtech.usingpaystack;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(
+        classes = {
+                UsingPaystackApplication.class,
+                UsingPaystackApplicationTests.TestConfig.class
+        }
+)
 class UsingPaystackApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        @ServiceConnection
+        static MySQLContainer<?> mysqlContainer() {
+            return new MySQLContainer<>(DockerImageName.parse("mysql:latest"));
+        }
     }
 
 }
