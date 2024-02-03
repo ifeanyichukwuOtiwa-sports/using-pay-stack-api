ebfdd4a7f9ec1fe82397b1d19d8d0364031279f7
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2024-02-03 19:03:11 +0100

    initial commit

---
+++ src/main/java/io/wtech/usingpaystack/UsingPaystackApplication.java
@@ -0,0 +1,13 @@
+package io.wtech.usingpaystack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingPaystackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingPaystackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/wtech/usingpaystack/UsingPaystackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wtech.usingpaystack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingPaystackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
